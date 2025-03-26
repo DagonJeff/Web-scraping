d5271cc3ee791192b73bfbc28c5bfdd3f670138a
DagonJeff <jefferson.dagon@icloud.com>
2025-03-25 23:28:43 -0300

    Scraper, downloads methods

--- src/main/java/com/download/Scraper.java
+++ src/main/java/com/download/Scraper.java
@@ -1,5 +1,58 @@
 package com.download;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.*;
+
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+
 public class Scraper {
+	
+	public static void downloadAnexos(String url, String downDir) throws IOException{
+		
+		System.out.println("Acessando " + url);
+		Document doc = Jsoup.connect(url).get();
+		System.out.println("Baizando arquivos...");
+		Elements links = doc.select("a[href$=.pdf]");
+		
+		for(Element link : links) {
+			String pdfUrl = link.attr("href");
+			String fileName = pdfUrl.substring(pdfUrl.lastIndexOf('/')+1);
+			
+			if(fileName.contains("Anexo I")|| fileName.contains("Anexo II")) {
+				download(pdfUrl, downDir);
+				System.out.println("Arquivo :" + fileName + " baixado com sucesso!");
+			}
+					
+		}
+		
+	}
+	private static void download(String pdfUrl, String downloadDir) throws IOException{
+		
+		URL url = new URL(pdfUrl);
+		HttpURLConnection conect = (HttpURLConnection) url.openConnection();
+		int responseCode = conect.getResponseCode();
+		
+		if(responseCode == HttpURLConnection.HTTP_OK) {
+			try(InputStream inps = conect.getInputStream()){
+				
+				String fileName = pdfUrl.substring(pdfUrl.lastIndexOf('/')+1);
+				Path otpPath = Paths.get(downloadDir, fileName);
+				Files.createDirectories(otpPath.getParent());
+				Files.copy(inps, otpPath);
+			}
+			
+		}else throw new IOException("Falha ao baixar arquivo. Código: "+ responseCode);
+		
+		
+	}
+	
 
 }
--- src/main/java/com/webscraping/WebScraping.java
+++ src/main/java/com/webscraping/WebScraping.java
@@ -1,5 +1,24 @@
 package com.webscraping;
 
 public class WebScraping {
+	
+	private static final String URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
+	private static final String DOWNLOAD_DIRECTORY = "donwloads/";
+	private static final String ZIP_PATH = "arquivos_anexados.zip";
+	
+	
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}finally {
+			
+		}
+		
+	}
 
 }
