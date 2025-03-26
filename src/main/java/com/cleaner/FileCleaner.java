4cdd9fa7553d06b646a86af759094235b75b8979
DagonJeff <jefferson.dagon@icloud.com>
2025-03-26 00:02:07 -0300

    Cleaner

--- src/main/java/com/cleaner/FileCleaner.java
+++ src/main/java/com/cleaner/FileCleaner.java
@@ -1,5 +1,26 @@
 package com.cleaner;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileCleaner {
+	
+	public static void cleanUp(Path dir) {
+		
+		try {
+			Files.walk(dir).filter(Files::isRegularFile).forEach(path -> {
+				try {	
+					Files.delete(path);
+					
+				} catch (Exception e) {
+					System.err.println("Falha ao tentar deletar: "+path.getFileName()+" "+ e.getMessage());
+				}
+			});
+			Files.deleteIfExists(dir);
+			
+		} catch (Exception e) {
+			System.err.println("Falha ao tentar limpar o diretórtio "+e.getMessage());
+		}
+	}
 
 }
